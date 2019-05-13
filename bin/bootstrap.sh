5955698d3c39f0b936d28d2ed389c212016a8548
Mithrand <mithrand0@users.noreply.github.com>
2019-05-13 20:37:59 +0200

    - include sourcemod admin and sourcebans

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# line
+echo '******************************************************************************'
+echo '* Starting Reactive Drop server                                              *'
+echo '******************************************************************************'
+
 # vars
 SLEEP_TIME=10
 
@@ -20,6 +25,44 @@ export WINEARCH="win32"
 export WINEDEBUG="-all"
 export WINEDLLOVERRIDES="mscoree=d;mshtml=d"
 #export WINEDEBUG="+synchronous,+loaddll,+seh,+msgbox,+fixme"
+
+function write_server_config()
+{
+    file=$1
+
+    # load the server.cfg file
+    echo "exec server.cfg" > $file
+
+    # look for other settings for this server
+    for vars in $(set | grep "^rd\_server\_${nr}\_"); do
+        var=$(echo "${vars}" | cut -d '_' -f 4- | cut -d '=' -f 1)
+        value=$(echo "${vars}" | cut -d '=' -f 2- | sed "s/\\\\'/\#/g" | sed "s/'//g" | sed "s/#/'/g")
+
+        # write the var the the server config file
+        echo "${var} = ${value}" >> $file
+    done
+}
+
+function write_sourcemod_admins_simple()
+{
+    if [[ "$sourcemod_admin_steamid" != "" ]]; then
+        echo "\"$sourcemod_admin_steamid\" \"z\"" > reactivedrop/addons/sourcemod/configs/admins_simple.ini
+    fi
+
+}
+
+function write_sourcemod_sourcebans_config()
+{
+    file="addons/sourcemod/configs/databases.cfg"
+
+    if [[ "${sourcebans_host}" != "" ]]; then
+        sed -i'' "s/#sourcebans_host#/${sourcebans_host}/g" $file
+        sed -i'' "s/#sourcebans_port#/${sourcebans_port}/g" $file
+        sed -i'' "s/#sourcebans_user#/${sourcebans_user}/g" $file
+        sed -i'' "s/#sourcebans_password#/${sourcebans_password}/g" $file
+        sed -i'' "s/#sourcebans_database#/${sourcebans_database}/g" $file
+    fi
+}
 
 # run a persistent wine server during initialization
 /usr/bin/wineserver -k -p 60
@@ -53,20 +96,14 @@ while [[ true ]]; do
             console="console_${nr}.log"
             file="reactivedrop/cfg/${config}"
 
-            # load the server.cfg file
-            echo "exec server.cfg" > $file
-
-            # look for other settings for this server
-            for vars in $(set | grep "^rd\_server\_${nr}\_"); do
-                var=$(echo "${vars}" | cut -d '_' -f 4- | cut -d '=' -f 1)
-                value=$(echo "${vars}" | cut -d '=' -f 2- | sed "s/\\\\'/\#/g" | sed "s/'//g" | sed "s/#/'/g")
-
-                # write the var the the server config file
-                echo "${var} = ${value}" >> $file
-            done
+            # write some configs
+            write_server_config "${file}"
+            write_sourcemod_admins_simple
+            write_sourcemod_sourcebans_config
 
             # create a copy of the sourcemod folder
-            cp -a reactivedrop/addons/sourcemod reactivedrop/addons/sourcemod_$i
+            smbase="reactivedrop/addons/sourcemod_${i}"
+            cp -a reactivedrop/addons/sourcemod $smbase
 
             # check if the env does exist
             wine start \
@@ -79,8 +116,8 @@ while [[ true ]]; do
             	-nocrashdialog \
                 -num_edicts 4096 \
                 +con_logfile $console \
-                +exec $config
-                +sm_basepath addons/sourcemod_$i
+                +exec $config \
+                +sm_basepath $smbase
 
             # wait a bit before attempting to start the next server
             sleep $SLEEP_TIME
